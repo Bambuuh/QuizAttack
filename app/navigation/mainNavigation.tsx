0490b69e00f4ff2e28c0918b45073cf0e407a931
Lukas Klingberg <lukas@klingberg.se>
2021-05-25 14:09:26 +0200

    basic theme

--- app/navigation/mainNavigation.tsx
+++ app/navigation/mainNavigation.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
+import { colors } from '../theme'
 import { StackRoute } from './constants'
 import MainStack from './home'
 
@@ -8,7 +9,17 @@ const { Navigator, Screen } = createStackNavigator()
 
 const MainNavigation: React.FC = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={{
+      dark: true,
+      colors: {
+        notification: colors.primary.color,
+        border: colors.primary.color,
+        background: colors.background.color,
+        card: colors.primary.color,
+        primary: colors.primary.color,
+        text: colors.background.onColor
+      }
+    }}>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name={StackRoute.MAIN} component={MainStack} />
       </Navigator>
--- app/theme/theme.ts
+++ app/theme/theme.ts
@@ -1,1 +1,11 @@
 export const baseline = 16
+export const colors = {
+  background: {
+    color: '#344448',
+    onColor: 'white',
+  },
+  primary: {
+    color: '#4F5e78',
+    onColor: 'white',
+  },
+}
